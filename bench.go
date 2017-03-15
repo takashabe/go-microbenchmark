8927b882d553d5d444328f1dad55ab9f4b05aa53
takashabe <takashabe.public@gmail.com>
2017-03-15 20:19:19 +0900

    add adhoc benchmark function and add begin/end function

--- bench.go
+++ bench.go
@@ -2,19 +2,46 @@ package microbenchmark
 
 import "time"
 
-type Benchmark struct{}
+// Benchmark object
+type Benchmark struct {
+	// get begin/end time in used benchmark
+	beginFunc func() time.Time
+	endFunc   func() time.Time
 
-func NewBenchmark() *Benchmark {
-	return &Benchmark{}
+	// benchmark start time
+	start time.Time
 }
 
+var defaultFunc = func() time.Time {
+	return time.Now()
+}
+
+// Create default benhcmark object
+func NewBenchmark() *Benchmark {
+	return &Benchmark{
+		beginFunc: defaultFunc,
+		endFunc:   defaultFunc,
+	}
+}
+
+// measure args functions. return all functions sum elapsed time
 func (b *Benchmark) Benchmark(args ...func()) (result time.Duration) {
-	start := time.Now()
+	b.start = b.beginFunc()
 	defer func() {
-		result = time.Since(start)
+		result = b.endFunc().Sub(b.start)
 	}()
 	for _, fn := range args {
 		fn()
 	}
 	return result
+}
+
+// begin benchmark
+func (b *Benchmark) Begin() {
+	b.start = b.beginFunc()
+}
+
+// end benchmark, return since Begin() to End() elapsed time
+func (b *Benchmark) End() time.Duration {
+	return b.endFunc().Sub(b.start)
 }
--- bench_test.go
+++ bench_test.go
@@ -5,10 +5,11 @@ import (
 	"time"
 )
 
-func TestElapsedTime(t *testing.T) {
-	sleep := func() {
-		time.Sleep(time.Millisecond)
-	}
+var testSleep = func() {
+	time.Sleep(time.Millisecond)
+}
+
+func TestElapsedTimeWithBenchmark(t *testing.T) {
 	cases := []struct {
 		input []func()
 		// got between expect and expectLimit
@@ -17,12 +18,12 @@ func TestElapsedTime(t *testing.T) {
 		expectLimit time.Duration
 	}{
 		{
-			[]func(){sleep},
+			[]func(){testSleep},
 			time.Millisecond,
 			time.Millisecond * 2,
 		},
 		{
-			[]func(){sleep, sleep, sleep, sleep},
+			[]func(){testSleep, testSleep, testSleep, testSleep},
 			time.Millisecond * 4,
 			time.Millisecond * 4 * 2,
 		},
@@ -30,6 +31,38 @@ func TestElapsedTime(t *testing.T) {
 	for i, c := range cases {
 		bench := NewBenchmark()
 		got := bench.Benchmark(c.input...)
+		if !(c.expect < got && got < c.expectLimit) {
+			t.Errorf("#%d: want %v ~ %v, got %v", i, c.expect, c.expectLimit, got)
+		}
+	}
+}
+
+func TestElapsedTimeWithBeginEnd(t *testing.T) {
+	cases := []struct {
+		fnList []func()
+		// got between expect and expectLimit
+		// WARNING: depends time.Now()
+		expect      time.Duration
+		expectLimit time.Duration
+	}{
+		{
+			[]func(){testSleep},
+			time.Millisecond,
+			time.Millisecond * 2,
+		},
+		{
+			[]func(){testSleep, testSleep, testSleep, testSleep},
+			time.Millisecond * 4,
+			time.Millisecond * 4 * 2,
+		},
+	}
+	for i, c := range cases {
+		bench := NewBenchmark()
+		bench.Begin()
+		for _, fn := range c.fnList {
+			fn()
+		}
+		got := bench.End()
 		if !(c.expect < got && got < c.expectLimit) {
 			t.Errorf("#%d: want %v ~ %v, got %v", i, c.expect, c.expectLimit, got)
 		}
